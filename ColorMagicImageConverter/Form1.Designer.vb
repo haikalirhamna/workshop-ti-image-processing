3815da9488c98e944700c7250a4d6085546e31d8
haikalirhamna <haikalirhamna15@gmail.com>
2023-07-29 23:06:29 +0700

    init

---
+++ ColorMagicImageConverter/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1340, 653)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 671)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(99, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "Load Image"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(117, 671)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 3
+        Button2.Text = "Converter"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(217, 671)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 4
+        Button3.Text = "Save Image"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1379, 721)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ ColorMagicImageConverter/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Using openFileDialog As New OpenFileDialog()
+            openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png;*.gif"
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                PictureBox1.Image = Image.FromFile(openFileDialog.FileName)
+            End If
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If PictureBox1.Image IsNot Nothing Then
+            Dim Image As Bitmap = ConvertImage(CType(PictureBox1.Image, Bitmap))
+            PictureBox1.Image = Image
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If PictureBox1.Image IsNot Nothing Then
+            Dim ConverterImage As Bitmap = ConvertImage(CType(PictureBox1.Image, Bitmap))
+            SaveConverted("test.bmp", ConverterImage)
+        End If
+    End Sub
+
+    Private Function ConvertImage(originalImage As Bitmap) As Bitmap
+        Dim newImage As New Bitmap(originalImage.Width, originalImage.Height)
+
+        Dim partWidth As Integer = originalImage.Width \ 3
+
+        For y As Integer = 0 To originalImage.Height - 1
+            For x As Integer = 0 To originalImage.Width - 1
+                Dim partIndex As Integer = x \ partWidth ' Determine the part index based on the x-coordinate
+
+                Dim originalColor As Color = originalImage.GetPixel(x, y)
+                Dim newColor As Color
+
+                Select Case partIndex
+                    Case 0
+                        newColor = Color.FromArgb(originalColor.A, originalColor.R, 0, 0)
+                    Case 1
+                        newColor = Color.FromArgb(originalColor.A, 0, originalColor.G, 0)
+                    Case 2
+                        newColor = Color.FromArgb(originalColor.A, 0, 0, originalColor.B)
+                    Case Else
+                        newColor = originalColor
+                End Select
+
+                newImage.SetPixel(x, y, newColor)
+            Next
+        Next
+
+        Dim watermarkText As String = "M. Haikal Irhamna"
+        Dim watermarkFont As New Font("Arial", 16)
+        Dim watermarkBrush As New SolidBrush(Color.White)
+        Dim watermarkPosition As New PointF(10, 10)
+
+        Using watermarkGraphics As Graphics = Graphics.FromImage(newImage)
+            watermarkGraphics.DrawString(watermarkText, watermarkFont, watermarkBrush, watermarkPosition)
+        End Using
+
+        Return newImage
+    End Function
+    Private Sub SaveConverted(filename As String, convertedImage As Bitmap)
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = "Bitmap Image|*.bmp"
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            convertedImage.Save(saveFileDialog.FileName, Imaging.ImageFormat.Bmp)
+        End If
+    End Sub
+
+
+End Class
